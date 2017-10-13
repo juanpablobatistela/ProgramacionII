d146ecaed20dc32882af64e2c55b3eb63f4807bf
Ariel Parra <arielp@eventbrite.com>
2017-10-12 22:21:18 -0300

    haciendo un cambio pequeño II

--- src/ejercicios/TrabajoPractico5.java
+++ src/ejercicios/TrabajoPractico5.java
@@ -13,10 +13,10 @@ import javax.swing.JPanel;
 
 /**
  * Interfaces Swing
- * @author arielp
+ * Otro cambio
  */
 public class TrabajoPractico5 {
-    
+
     public void ejercicio1() {
         JFrame frame = new JFrame();
         frame.setLayout(null);
@@ -24,23 +24,23 @@ public class TrabajoPractico5 {
         frame.setTitle("Trabajo Practico 5");
         frame.setVisible(true);
     }
-    
+
     public void ejercicio2() {
         JFrame frame = new JFrame();
         frame.setLayout(null);
         frame.setBounds(20, 20, 600, 800);
         frame.setTitle("Trabajo Practico 5");
         frame.setVisible(true);
-        
+
         JInternalFrame internalFrame = new JInternalFrame();
         internalFrame.setTitle("Internal Frame");
         internalFrame.setBackground(Color.red);
         internalFrame.setBounds(10, 20, 400, 400);
-        
+
         frame.add(internalFrame);
         internalFrame.setVisible(true);
     }
-    
+
     /**
      * Construir un Frame con titulo "Ejercicio 3", e incluir un internal frame
      * con titulo "Interno".
@@ -52,32 +52,32 @@ public class TrabajoPractico5 {
         ventanaPrincipal.setVisible(true);
         ventanaPrincipal.setTitle("Ejercicio 3");
         ventanaPrincipal.setLayout(null);
-        
+
         JInternalFrame ventanaInterna = new JInternalFrame();
         ventanaInterna.setBounds(0, 0, 200, 200);
         ventanaInterna.setTitle("Ventana Interna");
         ventanaInterna.setBackground(Color.BLUE);
-        
+
         ventanaPrincipal.add(ventanaInterna);
         ventanaInterna.setVisible(true);
-        
-        JInternalFrame ventanaInterna2 = new JInternalFrame();        
+
+        JInternalFrame ventanaInterna2 = new JInternalFrame();
         ventanaInterna2.setBounds(0, 200, 200, 200);
         ventanaInterna2.setTitle("Ventana Interna 2");
         ventanaInterna2.setBackground(Color.GREEN);
-        
+
         ventanaPrincipal.add(ventanaInterna2);
         ventanaInterna2.setVisible(true);
-        
+
         JPanel panel = new JPanel();
         panel.setBackground(Color.yellow);
         panel.setBounds(400, 200, 200, 200);
         ventanaPrincipal.add(panel);
-        
+
         JButton boton1 = new JButton();
         boton1.setBounds(200, 200, 200, 50);
         boton1.setText("Cerrar Ventanas");
-                    
+
         boton1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -89,15 +89,15 @@ public class TrabajoPractico5 {
                     System.out.println("Excepcion");
                 }
             }
-        });    
-        
+        });
+
         panel.add(boton1);
         boton1.setVisible(true);
-        
+
         JButton boton2 = new JButton();
         boton2.setBounds(100, 600, 200, 50);
         boton2.setText("Abrir Ventanas");
-        
+
         boton2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -105,9 +105,9 @@ public class TrabajoPractico5 {
                 ventanaInterna2.setVisible(true);
             }
         });
-        
+
         panel.add(boton2);
         boton2.setVisible(true);
-        
+
     }
 }
